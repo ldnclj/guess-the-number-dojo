45cb4157d06dc5e290c9ab7b99e8de1e8c7f31d0
mark <mark@entro-1>
2017-09-26 19:56:20 +0100

    daily commit

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject guess-the-number-dojo "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :main ^:skip-aot guess-the-number-dojo.core
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
--- src/guess_the_number_dojo/core.clj
+++ src/guess_the_number_dojo/core.clj
@@ -1,6 +1,17 @@
-(ns guess-the-number-dojo.core)
+(ns guess-the-number-dojo.core(:gen-class))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+
+(defn play-as-guesser "" [] nil )
+
+(defn -main
+  [& args]
+  (println "" ) 
+  )
+
+
+
