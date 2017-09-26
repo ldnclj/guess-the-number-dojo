3f7f54329aa57459e0692214b8d800854e78ca95
mark <mark@entro-1>
2017-09-26 20:30:29 +0100

    daily commit

--- src/guess_the_number_dojo/core.clj
+++ src/guess_the_number_dojo/core.clj
@@ -1,17 +1,35 @@
-(ns guess-the-number-dojo.core(:gen-class))
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(ns guess-the-number-dojo.core (:gen-class))
 
 
-(defn play-as-guesser "" [] nil )
+(defn ask-for-feedback []
+  (loop
+   []
+    (println "Tell me am I right (y)es, or is your number l(ower), h(higher) than my guess? ")
+    (let [line  (read-line)]
+      (cond (re-matches #"y|h|l" line) line
+            :default (recur)))))
+
+;; guesses so far to be added
+(defn guess-to-limit [lowbound highbound counter]
+  (let
+    [guess (quot (+ lowbound highbound) 2)
+     _ (println "My guess is " guess)
+     feedback (ask-for-feedback)]
+     (if (= feedback "y")
+       counter
+       (let
+           [[new-higher new-lower] (if (= feedback "h") [highbound (inc guess)] [guess lowbound])]
+         (recur new-lower new-higher (inc counter))))))
+
+(defn player-as-chooser "" [limit]
+  (println "Choose a whole number from 1 to" limit "and I will guess")
+
+  (println "Hit enter when ready")
+  (read-line)
+  (let [counter (guess-to-limit 1 limit 1)]
+    (println "got it in" counter "guesses")))
 
 (defn -main
   [& args]
-  (println "" ) 
-  )
-
-
+  (player-as-chooser 10))
 
